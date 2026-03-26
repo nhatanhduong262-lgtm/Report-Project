8e50ab777fb05b00188a2d32798c43cae778fce4
nhatanhduong262-glmt <nhatanhduong262@gmail.com>
2026-03-26 13:22:55 +0700

    Hoàn thành toàn bộ Core Backend (Auth, Catalog, Cart, Order) và fix lỗi JSON

---
+++ Backend/BadHabits.API/BadHabits.API/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BadHabits.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BadHabits.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // Dòng này sẽ tạo ra một bảng tên là "Users" trong SQL Server
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+         public DbSet<Product> Products { get; set; }
+         public DbSet<Variant> Variants { get; set; }
+
+        // ĐÂY LÀ 2 BẢNG MỚI CHÚNG TA VỪA THÊM:
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        // THÊM 2 BẢNG NÀY CHO GIỎ HÀNG:
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+    }
+}
