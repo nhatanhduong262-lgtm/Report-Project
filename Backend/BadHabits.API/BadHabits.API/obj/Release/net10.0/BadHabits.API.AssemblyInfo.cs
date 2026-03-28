bb8d4753eae730c0ebd570c27a6cfeb1d17a0692
nhatanhduong262-lgtm <nhatanhduong262@gmail.com>
2026-03-28 19:47:02 +0700

    update source code 3/28/2026

--- Backend/BadHabits.API/BadHabits.API/obj/Release/net10.0/BadHabits.API.AssemblyInfo.cs
+++ Backend/BadHabits.API/BadHabits.API/obj/Release/net10.0/BadHabits.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BadHabits.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bfd122846d7f5071be472abe80d4925e0fce73a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BadHabits.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BadHabits.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
